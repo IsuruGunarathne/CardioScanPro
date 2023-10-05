16ecbaa40f4d58296189bc2c63d446cf508c6636
sulakshi <gunarathneawsm.20@uom.lk>
2023-10-05 15:00:45 +0530

    dataframe creating function

--- utils-preprocessing.py
+++ utils-preprocessing.py
@@ -39,3 +39,44 @@ def create_array(hea_content):
 
 
 
+#===================================================================================================
+def create_dataframes(training_directory):
+    dataframes = {}
+
+    subdirectories = [subdir for subdir in pl.Path(training_directory).iterdir() if subdir.is_dir()]
+    
+    for source_folder_path in subdirectories:
+        source_folder_name = source_folder_path.name
+        columns = ['ID', 'Age', 'Gender', 'Abnormality']
+        source_dataframe = pd.DataFrame(columns=columns)
+        patient_data = {}  # To collect patient information
+        
+        for subdir in source_folder_path.iterdir():
+            if subdir.is_dir():
+                data_dir = pl.Path(subdir)
+                header_files = list(data_dir.glob('*.hea'))
+
+                for header_file in header_files:
+                    header_path = data_dir.joinpath(header_file.name)
+                    hea_content = read_heafile(header_path)
+                    patient_info = create_array(hea_content)
+                    patient_id = patient_info[0]
+                    
+                    # Collect patient information
+                    for i, column_name in enumerate(['Age', 'Gender', 'Abnormality']):
+                        patient_data.setdefault(patient_id, {})[column_name] = patient_info[i + 1]
+                        
+        # Create a list of patient data dictionaries
+        patient_rows = []
+        for patient_id, info in patient_data.items():
+            row = {'ID': patient_id, 'Age': info.get('Age'), 'Gender': info.get('Gender'), 'Abnormality': info.get('Abnormality')}
+            patient_rows.append(row)
+            
+        # Concatenate patient data into the dataframe
+        source_dataframe = pd.concat([source_dataframe, pd.DataFrame(patient_rows)])
+        
+        dataframes[f'{source_folder_name}_df'] = source_dataframe
+        
+    return dataframes
+
+
